9139eba946c46f75af9508b6fa15cf9ecd8c3909
Vagner Lucas <vagnerlucas@hotmail.com>
2018-01-09 09:14:24 -0300

    xml improvements

--- tests/xml/xmlwritter.test.js
+++ tests/xml/xmlwritter.test.js
@@ -2,10 +2,19 @@ const { expect } = require('chai');
 const XmlWritter = require('../../lib/xml/writter');
 
 describe('XmlWritter', () => {
-  it('should create a sitemap.xml structure', () => {
+  it('should create a urlset structure', () => {
     const xmlWritter = new XmlWritter();
     xmlWritter.addToUrlSet('test', 'daily');
     xmlWritter.addToUrlSet('test2', 'monthly', '1.0', true);
+    xmlWritter.close();
+    // console.log(xmlWritter.data);
+    return expect(xmlWritter.data).to.not.be.null;
+  });
+
+  it('should create a sitemapindex structure', () => {
+    const xmlWritter = new XmlWritter();
+    xmlWritter.addToSitemapIndex('test', null, null, true);
+    xmlWritter.addToSitemapIndex('test2', 'monthly', '1.0', true);
     xmlWritter.close();
     // console.log(xmlWritter.data);
     return expect(xmlWritter.data).to.not.be.null;
