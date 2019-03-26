37774b3f251b84799aa2dea32c0b0cdfb1662f92
Vagner Lucas <vagnerlucas@hotmail.com>
2019-03-26 14:11:03 -0300

    first commit

--- tests/database/database.test.js
+++ tests/database/database.test.js
@@ -2,9 +2,9 @@ const { expect } = require('chai');
 const DatabaseHelper = require('../../lib/db/database');
 
 describe('DatabaseHelper', () => {
-  it('should get some data from database', async () => {
-    const dbHelper = new DatabaseHelper();
-    const data = await dbHelper.fetchGenericCourseRoutes();
-    return expect(data).not.to.be.null;
-  });
+    it('should get some data from database', async() => {
+        const dbHelper = new DatabaseHelper();
+        const data = await dbHelper.fetchGenericCourseRoutes();
+        return expect(data).not.to.be.null;
+    });
 });
